792b8a431520ce8eebfb1ae2ba5ae54b066b7929
SanderVanOff <7okay@mail.ru>
2021-11-26 21:07:45 +0500

    второе домашнее задание

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaprive;
+//объявление переменных
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 50;
+let fullPrice = 20000;
+let adaprive = true;
 
-alert('Переменные объявлены');
+// alert('Переменные объявлены');
 
-console.log('Сообщение с любым текстом');
-
+//Типы данных
+console.log("type of title:", typeof title);
+console.log("type of fullPrice:", typeof fullPrice);
+console.log("type of adaprive:", typeof adaprive);
+//длина строки
+console.log("screens length:", screens.length);
+//Стоимость верстки экранов
+console.log(
+  "Cтоимость верстки эрканов:",
+  `${screenPrice} руб. /`,
+  `${(screenPrice / 76).toFixed(1)} $. /`,
+  `${(screenPrice / 2.76).toFixed(1)} ₴. /`,
+  `${(screenPrice / 11.68).toFixed(1)} ¥.`
+);
+console.log(
+    "Стоимость разработки сайта:",
+    `${fullPrice} руб. /`,
+    `${(fullPrice / 76).toFixed(1)} $. /`,
+    `${(fullPrice / 2.76).toFixed(1)} ₴. /`,
+    `${(fullPrice / 11.68).toFixed(1)} ¥.`
+  );
+//
+console.log("screens", screens.toLowerCase().split(","));
+//
+console.log("откат", fullPrice * (rollback / 100));
